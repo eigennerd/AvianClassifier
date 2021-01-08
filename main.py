4f6f0bb06ba5151f183a183ace1b9c0244a361b8
Daniel Einor <deino@softserveinc.com>
2021-01-08 13:26:52 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#from engine.read_data import *
+#from engine.model import *
+import streamlit as st
+import json
+with open('engine/config.json') as conf_file: # load JSON config file
+    configGlobal = json.load(conf_file)
+    texts = configGlobal['demo']
+
+header = st.header(texts['header_1'])
+header_text = st.write(texts['intro_1'])
+
+if st.sidebar.checkbox("Press to Begin"):
+    st.write(st.__version__)
