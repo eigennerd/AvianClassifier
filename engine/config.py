3a9d7051cf8577d433d8d7b65eaf4099889a4436
eigennerd <40917298+eigennerd@users.noreply.github.com>
2021-02-20 11:38:30 +0100

    Update config.py

--- engine/config.py
+++ engine/config.py
@@ -23,6 +23,7 @@ dict={
         "success_msg": "AI угадал - ",
         "not_guessed_msg": "AI не угадал :(  ",
         "uploaded_msg": "Не знаю правильный ответ",
+        "low_certainty_msg": "* - Птицы с показателем вероятности <5% считаются неопознаными",
         "select_mp3": "Выбери мп3",
         "upload_mp3": "Загрузи свой мп3",
         "top_guess": "Это :",
@@ -34,6 +35,7 @@ dict={
         "success_msg": "AI вгадав - ",
         "not_guessed_msg": "AI не вгадав :(  ",
         "uploaded_msg": "Не знаю правильну відповідь",
+        "low_certainty_msg": "* - Зазначені птахи, що мають імовірність <5%, вважаються нерозпізнаними",
         "select_mp3": "Вибери мп3",
         "upload_mp3": "Загрузи свій мп3",
         "top_guess": "Це :",
@@ -44,29 +46,32 @@ dict={
         "intro_1": "Select from a set of test mp3's, or use widgets to upload or record your own birb - AI will attempt to guess it!",
         "success_msg": "AI guessed - ",
         "not_guessed_msg": "AI did not guess the birb: ",
+        "low_certainty_msg": "* - predictions with certainty <5 % considered unreliable",
         "uploaded_msg": "Ground Truth of uploaded files unknown",
         "select_mp3": "Select mp3 from validation batch",
         "upload_mp3": "Upload pre-recorded mp3"
   },
- "fr": {
-    "header_1": "Devinez votre oiseau - Demo",
-    "intro_1": "Faites votre choix parmi un ensemble de mp3 de test ou utilisez des widgets pour telecharger ou enregistrer votre propre oiseau - l'IA tentera de le deviner!",
-    "success_msg": "AI guessed - ",
-    "not_guessed_msg": "AI did not guess the birb: ",
-    "uploaded_msg": "Ground Truth of uploaded files unknown",
-     "select_mp3": "Select mp3 from validation batch",
-    "upload_mp3": "Upload pre-recorded mp3"
-  },
-  "pl": {
-    "header_1": "Co to za ptak?",
-    "intro_1": "Wybierz z zestawu testowych plikow MP3 lub uzyj widzetow, aby przeslac wlasnego MP3, lub nagraj wlasnego ptaka - AI sprobuje go odgadnac!",
-    "success_msg": "AI guessed - ",
-    "not_guessed_msg": "AI did not guess the birb: ",
-    "uploaded_msg": "Ground Truth of uploaded files unknown",
-    "select_mp3": "Select mp3 from validation batch",
-    "upload_mp3": "Upload pre-recorded mp3",
-      "top_guess": "Top guess: ",
-      "top_5": "Top 5 guesses: "
+   "fr": {
+      "header_1": "Devinez votre oiseau - Demo",
+      "intro_1": "Faites votre choix parmi un ensemble de mp3 de test ou utilisez des widgets pour telecharger ou enregistrer votre propre oiseau - l'IA tentera de le deviner!",
+      "success_msg": "AI guessed - ",
+      "not_guessed_msg": "AI did not guess the birb: ",
+      "low_certainty_msg": "* - predictions with certainty <5 % considered unreliable",
+      "uploaded_msg": "Ground Truth of uploaded files unknown",
+       "select_mp3": "Select mp3 from validation batch",
+      "upload_mp3": "Upload pre-recorded mp3"
+    },
+    "pl": {
+        "header_1": "Co to za ptak?",
+        "intro_1": "Wybierz z zestawu testowych plikow MP3 lub uzyj widzetow, aby przeslac wlasnego MP3, lub nagraj wlasnego ptaka - AI sprobuje go odgadnac!",
+        "success_msg": "AI guessed - ",
+        "not_guessed_msg": "AI did not guess the birb: ",
+        "uploaded_msg": "Ground Truth of uploaded files unknown",
+        "low_certainty_msg": "* - predictions with certainty <5 % considered unreliable",
+        "select_mp3": "Select mp3 from validation batch",
+        "upload_mp3": "Upload pre-recorded mp3",
+          "top_guess": "Top guess: ",
+          "top_5": "Top 5 guesses: "
   }
 }
 
